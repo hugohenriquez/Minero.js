1537cb32228759c0d3659b106cfb9a52a3858b77
peposhh <jo.winter0000@gmail.com>
2020-04-03 10:29:19 -0300

    first version admin mineral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { TreeviewComponent } from './treeview/treeview.component';
+import {MatTreeModule} from '@angular/material/tree';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    TreeviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatTreeModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
